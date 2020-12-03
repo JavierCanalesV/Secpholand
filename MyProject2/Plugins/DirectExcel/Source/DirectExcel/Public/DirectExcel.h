157487a2940f7bf5ca1a03aa024ac6bab1869cf8
ivan <vy>
2020-12-03 16:00:52 +0100

    Excel c++

---
+++ MyProject2/Plugins/DirectExcel/Source/DirectExcel/Public/DirectExcel.h
@@ -0,0 +1,18 @@
+// Copyright 2018 Jianzhao Fu. All Rights Reserved.
+
+#pragma once
+
+#include "Modules/ModuleManager.h"
+
+class FDirectExcelModule : public IModuleInterface
+{
+public:
+
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+
+private:
+	/** Handle to the test dll we will load */
+	void*	mLibraryHandle = nullptr;
+};
