f4000fd6d240b46e102373395ca9a5dac01909c7
ivan <vy>
2021-02-05 19:55:13 +0100

    clicable avatars rework

--- MyProject2/Source/MyProject2/Private/NetworkGameStateOverride.cpp
+++ MyProject2/Source/MyProject2/Private/NetworkGameStateOverride.cpp
@@ -5,34 +5,39 @@
 
 void ANetworkGameStateOverride::GetUserCpp(UPARAM(ref) TArray<FUserInfocpp> & userStruct, const int _ID, const FString Username, const FString UniqueID, const APlayerController* PlayerController, const TEnumAsByte<EUserSearchType> LookBye, bool & _found, struct FUserInfocpp & foundUser, int & Index)
 {
+	foundUser.UserName = "NONE";
+	_found = false;
+	Index = -1;
 	for (int i = 0; i < userStruct.Num(); i++) {
-
-		foundUser = userStruct[i];
-		Index = i; 
 		switch (LookBye)
 		{
 		case EUserSearchType::ID:
 			if (userStruct[i].ID == _ID) {
 				_found = true;
-				return;
+				Index = i;
+				break;
 			}
 			break;
 		case EUserSearchType::UserName:
 			if (userStruct[i].UserName == Username) {
 				_found = true;
-				return;
+				foundUser = userStruct[i];
+				Index = i;
+				break;
 			}
 			break;
 		case EUserSearchType::PlayerController:
 			if (userStruct[i].PlayerController == PlayerController) {
 				_found = true;
-				return;
+				Index = i;
+				break;
 			}
 			break;
 		case EUserSearchType::UniqueID:
 			if (userStruct[i].UniqueID == UniqueID) {
 				_found = true;
-				return;
+				Index = i;
+				break;
 			}
 			break;
 		default:
